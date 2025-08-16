1dd6b00d9747919c55212bef82d1f16ce52ebbb9
Breno Corsi <161238146+Breno-Corsi@users.noreply.github.com>
2025-08-16 18:05:08 -0300

    feat(script.js): function to input and output in textarea

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Foi");
+function getStringInput() {
+    let stringInput = document.getElementById("string-input").value;
+    return stringInput;
+}
+
+function showStringOutput(stringOutput) {
+    document.getElementById("string-output").value = stringOutput;
+}
+
+function compressString() {
+
+}
+
+function decompressString() {
+
+}
