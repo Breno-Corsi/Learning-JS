1e77faa9036043accfe0d8b8fb5acc90ab8a9357
Breno Corsi <161238146+Breno-Corsi@users.noreply.github.com>
2025-08-18 15:28:31 -0300

    feat(script.js): add input treatment

--- primeNumbers/script.js
+++ primeNumbers/script.js
@@ -1,12 +1,33 @@
+let startNumber = 0;
+let endNumber = 0;
+
 function showResult(result) {
     document.getElementById("output-number").value = result;
 }
 
 function getNumber(elementId) {
-    return Number(document.getElementById(elementId).value);
+    numberValue = Number(document.getElementById(elementId).value);
+    if (numberValue < 0) {
+        alert("No negative values allowed!");
+        return false;
+    } else if (numberValue > 10000000) {
+        alert("Max value allowed: 10.000.000");
+        return false;
+    }
+    return numberValue;
 }
 
 function calculateNumbers() {
     startNumber = getNumber("start-number");
     endNumber = getNumber("end-number");
+    swapIfSecondValueIsGreater();
+    console.time("calculateNumbers");
+    
+    console.timeEnd("calculateNumbers");
+}
+
+function swapIfSecondValueIsGreater() {
+    if (startNumber > endNumber) {
+        [startNumber, endNumber] = [endNumber, startNumber];
+    }
 }
