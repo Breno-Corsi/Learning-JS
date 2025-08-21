0e54157b060ee7acef32a4857e5c32eff47fdf70
Breno Corsi <161238146+Breno-Corsi@users.noreply.github.com>
2025-08-21 19:32:58 -0300

    feat(script.js): add func inputSymbol() (design only)

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,1 +1,8 @@
-console.log("a");
+const symbols = ["X", "O"];
+
+function inputSymbol(position, i, j) {
+    let p = document.getElementById(position);
+    console.log(position);
+    p.innerHTML = symbols[0];
+    [symbols[0], symbols[1]] = [symbols[1], symbols[0]];
+}
