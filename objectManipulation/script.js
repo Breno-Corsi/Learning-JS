d52a463452dc27e41d84b50508fcd46f612dfe82
Breno Corsi <161238146+Breno-Corsi@users.noreply.github.com>
2025-08-23 13:41:29 -0300

    feat(script.js): add func showValue()

---
+++ objectManipulation/script.js
@@ -0,0 +1,29 @@
+const abacate = document.getElementById("sim-terminal");
+
+function showValue(text) {
+    abacate.value = text
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Beginner Level
+// Count how many properties an object has.
+// Check if a specific key exists in an object.
+// List all keys of an object.
+// List all values of an object.
+// Create a function that returns a copy of the object.
+// Intermediate Level
+// Merge two objects into a new object.
+// Invert keys and values of an object.
+// Filter an object based on the value of its properties.
+// Remove a specific property from an object.
+// Create an object from two arrays: one of keys and another of values.
